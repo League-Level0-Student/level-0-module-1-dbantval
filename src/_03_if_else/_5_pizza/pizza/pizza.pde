a8cb2581d5508cd573d8f9f8423c58895f1813fe
root <root@25bdb4f0759e>
2020-04-29 23:59:52 +0000

    Finsished the riddler, and pizza

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,18 @@
 
 void setup() {
-    
+    size(2000,2000);fill(#FFD390);
+    ellipse(1000,270,500,500);
+    fill(#FF0320);
+    ellipse(1000,270,400,400);
+    fill(#FAF58B);
+    ellipse(1000,270,390,390);
 }
 void draw() {
-    
+PImage pepp=loadImage("Pepp.jpg");
+pepp.resize(70,70);
+image(pepp,1070,350);
+pepp.resize(50,50);
+image(pepp,900,300);
+pepp.resize(100,100);
+image(pepp,950,100);
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,31 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score =0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String answer= JOptionPane.showInputDialog("I have cities, but no houses. I have mountains, but no trees. I have water, but no fish. What am I?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if(answer.equalsIgnoreCase("a map")){ 
+	JOptionPane.showMessageDialog(null, "Correct!");
+	++score ;
+}
+	else { JOptionPane.showMessageDialog(null, "WROOONG! The answer was a map");
+		
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
-
+String answer2 = JOptionPane.showInputDialog("What is seen in the middle of March and April that can't be seen at the beginning or end of either month?");
+if(answer2.equalsIgnoreCase("The letter r")){
+	JOptionPane.showMessageDialog(null, "Coooooorrectttt!!!");
+	++score ;
+}
+else { JOptionPane.showMessageDialog(null, "INCORRECTTTTTT!");
+JOptionPane.showMessageDialog(null, "The answer was The letter r");
+	
+}
 		// 2. Make a pop up to show the score.
 		
-	}
-}
+	
+}}
 
