bf700857b2ffb6a9beaf7d44b9def492d7332cd0
root <root@b15cdbc446de>
2020-05-08 01:25:10 +0000

    Bager BAger Bager Banana Banana

---
+++ src/_05_for_loops/_2_badgers/Badger_Badg.java
@@ -0,0 +1,21 @@
+package _05_for_loops._2_badgers;
+
+import javax.swing.JOptionPane;
+
+public class Badger_Badg {
+	public static void main(String args[]){
+		for (int badger = 0; badger < 4; badger++) {
+			
+		
+	for (int i = 0; i < 12; i++) {
+System.out.print("Badger Badg ");
+}
+	System.out.println();
+	System.out.println("Mushroom Mushroom!");
+ 
+}
+		System.out.println();
+		System.out.println("A snake, a snake Snaaake! A snaaaake Oooh, it's a snake");
+	}
+}
+
---
+++ src/_05_for_loops/_3_bananas_4eva/bananas_4eva/bananas_4eva.pde
@@ -0,0 +1,5 @@
+size(700,700);
+for (int i = 0; i < 1000; i++) {
+  System.out.print("banana ");
+
+}
